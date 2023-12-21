3d099a5dcf1a53188b5eefbeb7b3f45f28ab0dfb
Sam12121 <smcoolsam44@gmail.com>
2023-12-21 15:15:21 +0530

    Update generate_roadmap.py

--- .github/scripts/generate_roadmap.py
+++ .github/scripts/generate_roadmap.py
@@ -6,6 +6,14 @@ from datetime import datetime, timedelta
 repo_owner = "Sam12121"
 repo_name = "demo"
 token = "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"
+import requests
+import json
+from datetime import datetime, timedelta
+
+# Replace with your GitHub repository details
+repo_owner = "your_username"
+repo_name = "your_repository"
+token = "your_personal_access_token"
 
 # Function to create a GitHub issue
 def create_issue(title, body, milestone_number):
@@ -18,8 +26,8 @@ def create_issue(title, body, milestone_number):
     response = requests.post(url, headers=headers, data=json.dumps(data))
     return response.json()
 
-# Function to create a GitHub milestone and extract milestone number
-def create_milestone_and_get_number(title, due_date=None):
+# Function to create a GitHub milestone
+def create_milestone(title, due_date=None):
     url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/milestones"
     headers = {
         "Authorization": f"Bearer {token}",
@@ -27,7 +35,7 @@ def create_milestone_and_get_number(title, due_date=None):
     }
     data = {"title": title, "due_on": due_date}
     response = requests.post(url, headers=headers, data=json.dumps(data))
-    milestone_number = response.json().get("number")
+    milestone_number = response.json().get("number")  # Extract milestone number
     return milestone_number
 
 # Replace with your roadmap details
@@ -40,12 +48,12 @@ roadmap = [
 ]
 
 # Calculate due dates based on the current date
-current_date = datetime.now(timezone.utc)
+current_date = datetime.utcnow()
 milestones = []
 
 for task in roadmap:
     due_date = current_date + timedelta(days=task["duration"] * 30)
-    milestone_number = create_milestone_and_get_number(task["title"], due_date.isoformat())
+    milestone_number = create_milestone(task["title"], due_date.isoformat())
     milestones.append(milestone_number)
 
 # Create issues under each milestone
